10350359dcef6de52a3163a400c28ed52bb6a76c
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-05-05 23:55:57 +0700

    Update IRepository interface, pass a predicate to select method.

--- src/main/java/com/group28/wwwjavafinal/infrastructure/database/IRepository.java
+++ src/main/java/com/group28/wwwjavafinal/infrastructure/database/IRepository.java
@@ -1,10 +1,11 @@
 package com.group28.wwwjavafinal.infrastructure.database;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public interface IRepository<T> {
 	List<T> selectAll();
-	T selectById(int id);
+	T select(Predicate<T> predicate);
 	boolean Add(T obj);
 	boolean Delete(T obj);
 	boolean Update(T obj);
